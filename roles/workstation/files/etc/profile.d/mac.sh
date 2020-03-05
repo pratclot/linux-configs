e0bc38bda6b555932156fca83bd874d1e583244a
chief <pratclot@live.ru>
2020-03-05 15:00:55 +0300

    make mac profile.d script safe for arch

--- roles/workstation/files/etc/profile.d/mac.sh
+++ roles/workstation/files/etc/profile.d/mac.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-source $(brew --prefix)/etc/bash_completion
-export HOMEBREW_NO_AUTO_UPDATE=1
+if [ $(command -v brew) ]; then
+  source $(brew --prefix)/etc/bash_completion
+  export HOMEBREW_NO_AUTO_UPDATE=1
+fi

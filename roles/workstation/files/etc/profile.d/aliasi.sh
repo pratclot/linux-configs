2f96876553c8f42232d6cfdb66fe1855484b7967
Sergei Stepanov <Sergei.Stepanov@noveogroup.com>
2021-06-29 17:24:03 +0300

    add hex alias

--- roles/workstation/files/etc/profile.d/aliasi.sh
+++ roles/workstation/files/etc/profile.d/aliasi.sh
@@ -36,6 +36,7 @@ alias urlencode='python3 -c "import sys, urllib.parse as ul; \
     print (ul.quote_plus(sys.argv[1]))"'
 alias gradlektsconverter=$(readlink -f `which gradlekotlinconverter`)
 alias gloww='while true; do clear; glow README.md; sleep 2; done'
+alias hex='printf "%x\n"'
 
 # kubectl
 alias kev='kubectl get events --sort-by="{.lastTimestamp}"'

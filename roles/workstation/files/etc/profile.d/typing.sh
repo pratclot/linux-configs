847d6d42b94c29cd77c5fc04f469b1223c935aaa
Sergei Stepanov <sergei.stepanov@deliveryhero.com>
2022-03-02 16:57:08 +0100

    Fix bash typing for mac

--- roles/workstation/files/etc/profile.d/typing.sh
+++ roles/workstation/files/etc/profile.d/typing.sh
@@ -2,3 +2,5 @@
 
 2>/dev/null bind '"\e[A": history-search-backward'
 2>/dev/null bind '"\e[B": history-search-forward'
+2>/dev/null bind '"\e\e[D": backward-word'
+2>/dev/null bind '"\e\e[C": forward-word'
